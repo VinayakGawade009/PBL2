4b039b2e29d2d0fea75ae06d6b43ad964bc9c4b4
VinayakGawade009 <vinayakgawade009@gmail.com>
2025-02-03 14:02:14 +0530

    Initial commit in PBL 2 Setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
+
+main().then(() => {
+    console.log("connection successful");
+})
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/codee');
+}
+
+app.get("/", (req, res) => {
+    res.send("Ani");
+});
+
+app.get("/test", async(req, res) => {
+    let sampleListing = new Listing ({
+        title: "Python Basics",
+        description: "Description of Python Basics Course.",
+        tag: "Python",
+        state: "recorded",
+        language: "english",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("sampleListing is saved");
+});
+
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default: "https://plus.unsplash.com/premium_photo-1681426687411-21986b0626a8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v==="" ? "https://plus.unsplash.com/premium_photo-1681426687411-21986b0626a8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    tag: String,
+    state: String,
+    language: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
