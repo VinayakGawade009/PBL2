c728c7372a282f455f30ad5e2de41832785480b9
VinayakGawade009 <vinayakgawade009@gmail.com>
2025-02-12 22:32:37 +0530

    Initialized Database and Created all CRUD operation routes for project.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
+const tags = require("./tags.js");
+const methodOverride = require("method-override");
 
 main().then(() => {
     console.log("connection successful");
@@ -15,15 +17,64 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/codee');
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
     res.send("Ani");
 });
 
-app.get("/listings", (req, res) => {
-    Listing.find({}).then(res => {
-        console.log(res);
-    });
-})
+// Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
+
+// New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs", { tags });
+});
+
+// Shouw Route
+app.get("/listings/:id", async (req, res) =>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+// Create Route
+app.post("/listings", async (req, res) => {
+    // extract all info
+    let newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    const prevValue = listing.tag;
+    res.render("listings/edit.ejs", { listing, tags, prevValue });
+
+});
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 // app.get("/test", async(req, res) => {
 //     let sampleListing = new Listing ({
---
+++ tags.js
@@ -0,0 +1,39 @@
+module.exports = [
+    { value: 'Full Stack', label: 'Full Stack' },
+    { value: 'Frontend', label: 'Frontend' },
+    { value: 'Backend', label: 'Backend' },
+    { value: 'Programming Language', label: 'Programming Language' },
+    { value: 'Programming', label: 'Programming' },
+    { value: 'Design', label: 'Design' },
+    { value: 'AI/ML', label: 'AI/ML' },
+    { value: 'Data Science', label: 'Data Science' },
+    { value: 'Cybersecurity', label: 'Cybersecurity' },
+    { value: 'Cloud', label: 'Cloud' },
+    { value: 'Blockchain', label: 'Blockchain' },
+    { value: 'DevOps', label: 'DevOps' },
+    { value: 'App Development', label: 'App Development' },
+    { value: 'Game Development', label: 'Game Development' },
+    { value: 'Networking', label: 'Networking' },
+    { value: 'DevOps', label: 'DevOps' },
+];
+
+
+// <label for="tag">Select a tag:</label>
+// <select name="listing[tag]" id="tag"">
+//     <option value="Full Stack">Full Stack</option>
+//     <option value="Frontend">Frontend</option>
+//     <option value="Backend">Backend</option>
+//     <option value="Programming Language">Programming Language</option>
+//     <option value="Programming">Programming</option>
+//     <option value="Design">Design</option>
+//     <option value="AI/ML">AI/ML</option>
+//     <option value="Data Science">Data Science</option>
+//     <option value="Cybersecurity">Cybersecurity</option>
+//     <option value="Cloud">Cloud</option>
+//     <option value="Blockchain">Blockchain</option>
+//     <option value="DevOps">DevOps</option>
+//     <option value="App Development">App Development</option>
+//     <option value="Game Development">Game Development</option>
+//     <option value="Networking">Networking</option>
+//     <option value="DevOps">DevOps</option>
+// </select>
